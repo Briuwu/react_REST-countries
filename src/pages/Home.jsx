366acff1583358231abdfddd80fec1df4e251fb5
Briuwu <millontebrian@gmail.com>
2023-01-19 18:16:18 +0800

    fix box shadow

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React from "react";
 import Countries from "../components/Countries/Countries";
 import Navigation from "../components/Navigation/Navigation";
 import { motion } from "framer-motion";
